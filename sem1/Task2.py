0fc642a8836a9c34ca4fb402b9f93126d6814de4
nemi <nemelida@bk.ru>
2023-04-14 18:56:51 +0300

    1sem hw

---
+++ sem1/Task2.py
@@ -0,0 +1,5 @@
+num_cranes=int(input("Напишите сколько жулавликов сделанно: "))
+num_cranes//=6
+print(f"Петя сделал журавликов: {num_cranes}")
+print(f"Катя сделал журавликов: {num_cranes}")
+print(f"Катя сделала журавликов: {num_cranes*4}")
